0d4ba5e1d3468ecb8f2c62658941056df1dde288
DmytryPan <panikhidin@sfedu.ru>
2026-02-18 16:25:41 +0300

    Add task with attention implementation

--- homeworks/hw02_attention_and_language_modeling/template_p01.py
+++ homeworks/hw02_attention_and_language_modeling/template_p01.py
@@ -14,29 +14,37 @@ def softmax(vector):
     return softmax_
 
 def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
-    '''
+    """
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     W_mult: np.array of shape (n_features_dec, n_features_enc)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
-    '''
+    """
     # your code here
-    
+    e = (decoder_hidden_state.T @ W_mult) @ encoder_hidden_states
+    weights = softmax(e)
+    attention_vector = encoder_hidden_states @ weights.T
     return attention_vector
 
-def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
-    '''
+def additive_attention(
+    decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec
+):
+    """
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     v_add: np.array of shape (n_features_int, 1)
     W_add_enc: np.array of shape (n_features_int, n_features_enc)
     W_add_dec: np.array of shape (n_features_int, n_features_dec)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
-    '''
+    """
     # your code here
-    
+    additive =  W_add_enc @ encoder_hidden_states + W_add_dec @ decoder_hidden_state
+    tanh_additive = np.tanh(additive)
+    e = v_add.T @ tanh_additive
+    weights = softmax(e)
+    attention_vector = encoder_hidden_states @ weights.T
     return attention_vector
